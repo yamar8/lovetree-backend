993cf4b3cfaf0336eb52b2817bef77669f9bb5f5
Yossef <yamar8@gmail.com>
2025-01-29 19:59:03 +0200

    revert

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay} from '../controllers/orderController.js'
+import {placeOrder, placeOrderStripe, allOrders, userOrders, updateStatus, verifyStripe, verifyRazorpay} from '../controllers/orderController.js'
 import adminAuth  from '../middleware/adminAuth.js'
 import authUser from '../middleware/auth.js'
 
@@ -12,7 +12,7 @@ orderRouter.post('/status',adminAuth,updateStatus)
 // Payment Features
 orderRouter.post('/place',authUser,placeOrder)
 orderRouter.post('/stripe',authUser,placeOrderStripe)
-orderRouter.post('/razorpay',authUser,placeOrderRazorpay)
+orderRouter.post('/razorpay',authUser)
 
 // User Feature 
 orderRouter.post('/userorders',authUser,userOrders)
